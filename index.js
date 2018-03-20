08d7835c953ab9407fd7496dbedd29d4ac11fbae
shofel <vistomin@examus.net>
2018-03-20 20:35:03 +0500

    src: fix the arg validation.

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-let isInteger = x => Number.isInteger(x)
+export let isInteger = x => Number.isInteger(x)
 
-let isDate = x =>
+let isDate_ = x =>
   ['getHours', 'getMinutes', 'getSeconds']
   .every(k => k in x)
 
+export let isDate = x => {
+  try { return isDate_(x) }
+  catch (_) { return false }
+}
+
 export default function gethhmmss (x) {
   /* Validate the arg. */
-  if (!isInteger(x) || !isDate(x)) {
+  if (!isInteger(x) && !isDate(x)) {
     throw (TypeError(`gethhmmss: expected integer or Date but got ${x}`))
   }
 }
