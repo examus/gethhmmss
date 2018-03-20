aad832dae4f7b534c452df90d369ea9d614e1261
shofel <vistomin@examus.net>
2018-03-20 21:54:33 +0500

    src: from seconds.

--- index.js
+++ index.js
@@ -9,9 +9,78 @@ export let isDate = x => {
   catch (_) { return false }
 }
 
-export default function gethhmmss (x) {
-  /* Validate the arg. */
-  if (!isInteger(x) && !isDate(x)) {
-    throw (TypeError(`gethhmmss: expected integer or Date but got ${x}`))
+let fromArray = ([hours, minutes, seconds]) => {
+  /* Expect only positive numbers. */
+
+  let allPositive =
+    [hours, minutes, seconds]
+    .every(x => x >= 0)
+
+  let allIntegers =
+    [hours, minutes, seconds]
+    .every(isInteger)
+
+  console.assert(
+    allPositive && allIntegers,
+    'gethhmmss: fromArray: expected positive integers but got',
+    `[${[hours, minutes, seconds].join(', ')}]`)
+
+  /* Do the job. */
+
+  return [hours, minutes, seconds]
+  .map(String)
+  .map(x => x.padStart(2, '0'))
+  .join(':')
+}
+
+let fromSeconds = givenSeconds => {
+  let positiveSeconds =
+    givenSeconds < 0
+      ? givenSeconds * -1
+      : givenSeconds
+
+  let [hours, minutes, seconds] = [
+    positiveSeconds / 3600,
+    positiveSeconds % 3600 / 60,
+    positiveSeconds % 60]
+  .map(x => Math.trunc(x))
+
+  let sign = givenSeconds < 0 ? '-' : ''
+  let hhmmss = fromArray([hours, minutes, seconds])
+
+  return ''.concat(sign, hhmmss)
+}
+
+let fromDate = date => {
+  let [hours, minutes, seconds] =
+    [date.getHours(), date.getMinutes(), date.getSeconds()]
+
+  let hhmmss =
+    fromArray([hours, minutes, seconds])
+
+  return hhmmss
+}
+
+export default function gethhmmss (...args) {
+
+  /**
+   * Validate the args.
+   */
+
+  if (args.length > 1) {
+    throw TypeError(`gethhmmss: zero or one argument expected but got [${args.join(', ')}]`)
   }
+
+  let [x] = args
+
+  if (args.length === 1 && !isInteger(x) && !isDate(x)) {
+    throw TypeError(`gethhmmss: expected integer or Date but got ${x}`)
+  }
+
+
+  /**
+   * From seconds.
+   */
+
+  if (isInteger(x)) return fromSeconds(x)
 }
